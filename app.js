e81f12c5370d898edb41b72ca34eff14bdf9cf38
Rudresh17 <35303211+Rudresh17@users.noreply.github.com>
2024-12-19 14:49:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ connectDB()
 //passport middleware
 const app = express()
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: (origin, callback) => {
+    callback(null, true); // Allow all origins dynamically
+  },
+  credentials: true,
+  optionSuccessStatus: 200
+};
 app.use(cors(corsOptions)); 
 app.use(cookieParser());
 //sessions middleware
